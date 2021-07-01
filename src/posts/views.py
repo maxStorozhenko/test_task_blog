7db0d9940d06b9aa170b07d02e54cbd980aae8d7
Maxim <maxistorozhenko@gmail.com>
2021-07-01 17:18:54 +0300

    Updates

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView, DetailView
 from .models import Post
-
+# some updates
 
 class PostsListView(ListView):
     queryset = Post.objects.filter(published=True).order_by('-pub_date')
